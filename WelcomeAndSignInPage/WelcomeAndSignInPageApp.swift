eff01bcc224d51715bd21087fd5400e8ae72df84
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-07 09:56:07 +0700

    Create the project

---
+++ WelcomeAndSignInPage/WelcomeAndSignInPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeAndSignInPageApp.swift
+//  WelcomeAndSignInPage
+//
+//  Created by Danh Tu on 07/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeAndSignInPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
