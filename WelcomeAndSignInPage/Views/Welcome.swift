f84d010187191715ce409da534ec5d18b08611bd
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-07 15:03:46 +0700

    Setting up project

---
+++ WelcomeAndSignInPage/Views/Welcome.swift
@@ -0,0 +1,20 @@
+//
+//  Welcome.swift
+//  WelcomeAndSignInPage
+//
+//  Created by Danh Tu on 07/10/2021.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Welcome_Previews: PreviewProvider {
+    static var previews: some View {
+        Welcome()
+    }
+}
